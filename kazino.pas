dafebb74568e7d41bce1b770793a38657d487236
Чирков <v.r.chirkov@gmail.com>
2025-09-10 06:12:40 +0300

    Some pascal files

---
+++ kazino.pas
@@ -0,0 +1,13 @@
+﻿var numb, stavka, color:int64;
+begin
+  writeln('Добро пожаловать в казино. Сделайте ставку: ');
+  readln(stavka);
+  color:=3;
+  while not (((color=0)or (color=1)) and (numb>=1)) do begin
+    writeln('Выберите число и цвет(0 - черный, 1 - красный): ');
+    readln(numb, color);
+  end;
+  if (random(0, 1)=color) and (random(1, 100)=numb) then writeln(stavka*10)
+  else if ((random(0, 1)<>color) and (random(1, 100)=numb)) or ((random(0, 1)=color) and (random(1, 100)<>numb)) then writeln(stavka*2)
+  else writeln(0);
+end.
---
+++ vozvedenie v stepen.pas
@@ -0,0 +1,16 @@
+﻿function stepen(n, st:int64):int64;
+var i, res:int64;
+begin
+  res:=1;
+  for i:=1 to st do begin
+    res*=n;
+  end;
+  stepen:=res;
+end;
+
+var n, st:int64;
+begin
+  write('Введите число и степень: ');
+  read(n, st);
+  writeln(stepen(n, st));
+end.
